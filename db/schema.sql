e628178edc15e4128fc792ce5436e1ec6613edb8
bkontos <brianrkontos@gmail.com>
2024-01-29 17:08:30 -0700

    Created database and added initial db schema

---
+++ db/schema.sql
@@ -0,0 +1,89 @@
+-- MySQL dump 10.13  Distrib 8.3.0, for Linux (x86_64)
+--
+-- Host: localhost    Database: ninoDB
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `items` (
+  `item_id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(100) DEFAULT NULL,
+  `size` enum('XS','small','medium','large','XL','XXL','XXXL','4X','5XL','6XL') DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `count_in` int DEFAULT NULL,
+  `count_out` int DEFAULT NULL,
+  `comps` int DEFAULT NULL,
+  `item_type` enum('hard','soft') DEFAULT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `sales_summary`
+--
+
+DROP TABLE IF EXISTS `sales_summary`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sales_summary` (
+  `summary_id` int NOT NULL AUTO_INCREMENT,
+  `tax_rate` decimal(5,2) DEFAULT NULL,
+  `total_gross` decimal(10,2) DEFAULT NULL,
+  `total_soft_gross` decimal(10,2) DEFAULT NULL,
+  `total_hard_gross` decimal(10,2) DEFAULT NULL,
+  `credit_card_fee` decimal(10,2) DEFAULT NULL,
+  `total_soft_net` decimal(10,2) DEFAULT NULL,
+  `total_hard_net` decimal(10,2) DEFAULT NULL,
+  `total_soft_owed_casino` decimal(10,2) DEFAULT NULL,
+  `total_hard_owed_casino` decimal(10,2) DEFAULT NULL,
+  `total_owed_casino` decimal(10,2) DEFAULT NULL,
+  `band_revenue_received` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`summary_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-30  0:02:57
