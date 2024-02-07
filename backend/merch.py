0cc49b61804dc31cb53efa8b1b38f1c8df99388e
bkontos <brianrkontos@gmail.com>
2024-02-06 17:41:06 -0700

    Setup flask migration and began process for Auth0

--- backend/merch.py
+++ backend/merch.py
@@ -1,32 +1,21 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    f"mysql+pymysql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}"
-    f"@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}"
-)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, directory= 'db/migrations')
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
-
-    def set_password(self, password):
-        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
-
-    def check_password(self, password):
-        return bcrypt.check_password_hash(self.password_hash, password)
+    auth0_id = db.Column(db.String(255), unique=True, nullable=False)  # New field for Auth0 user ID
 
 class InventoryItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -37,35 +26,6 @@ class InventoryItem(db.Model):
     count_out = db.Column(db.Integer)
     comps = db.Column(db.Integer)
     item_type = db.Column(db.String(50))
-
-@app.route('/signup', methods=['POST'])
-def signup():
-    data = request.json
-    username = data.get('username')
-    email = data.get('email')
-    password = data.get('password')
-
-    if not all([username, email, password]):
-        return jsonify({'error': 'Missing username, email, or password'}), 400
-
-    # Check if user already exists
-    if User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first():
-        return jsonify({'error': 'Username or email already exists'}), 409
-
-    # Create new user
-    new_user = User(username=username, email=email)
-    new_user.set_password(password)
-
-    # Add new user to the database
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({'message': 'User created successfully'}), 201
-
-@app.route('/login', methods=['POST'])
-def login():
-    # Logic for user login
-    return jsonify({'message': 'Login successful'})
 
 @app.route('/inventory', methods=['POST'])
 def add_item():
